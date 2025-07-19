ace6ce17041bd5837c949e55438ddf926420c660
William Young <wadyoung@outlook.com>
2025-07-19 17:49:31 +0100

    fix drift yaw lock

--- states/drifting.gd
+++ states/drifting.gd
@@ -4,7 +4,7 @@ class_name DriftingState
 var state_name = "drifting"
 
 func enter(character: Character, delta) -> void:
-	var drift_center_yaw = character.rotation.y
+	pass
 
 func exit(character: Character, delta) -> void:
 	pass
@@ -14,36 +14,7 @@ func update(character: Character, delta) -> void:
 	character.apply_character_gravity(delta)
 	character.move_and_slide()
 	character.apply_hover(delta)
-	
-	# holding a direction very slowly yaws the board in that direction
-	# This change of yaw changes the direction of the boards velocity
-	var drift_fine_yaw_rate := 0.5
-	const MAX_FINE_BANK = deg_to_rad(15)
-	print(character.base_jump_yaw)
-	if character.left_drift:
-		
-		if character.input_left:
-			character.rotation.y += drift_fine_yaw_rate * delta
-		elif character.input_right:
-			character.rotation.y = max(
-			character.rotation.y,
-			character.base_jump_yaw + MAX_FINE_BANK
-			)
-			character.rotation.y -= drift_fine_yaw_rate * delta
-			
-	elif character.right_drift:
-		# figure out how much to turn this frame:
-		
-		var delta_yaw = 0.0
-		if character.input_right:
-			character.rotation.y -= drift_fine_yaw_rate * delta
-		elif character.input_left:
-			character.rotation.y = max(
-				character.rotation.y,
-				character.base_jump_yaw + -MAX_FINE_BANK,
-			)
-			character.rotation.y += drift_fine_yaw_rate * delta
-
+	_apply_fine_drift_yaw(character, delta)
 
 
 func on_trigger(character: Character, trigger: int) -> State:
@@ -53,3 +24,26 @@ func on_trigger(character: Character, trigger: int) -> State:
 			character.right_drift = false
 			return GroundState.new()
 	return null
+
+func _apply_fine_drift_yaw(character: Character, delta):
+	var drift_fine_yaw_rate := 0.5
+	const MAX_FINE_BANK = deg_to_rad(15)
+
+	if character.left_drift:
+		if character.input_left:
+			character.rotation.y += drift_fine_yaw_rate * delta
+		elif character.input_right:
+			character.rotation.y = max(
+				character.rotation.y,
+				character.base_jump_yaw + MAX_FINE_BANK
+			)
+			character.rotation.y -= drift_fine_yaw_rate * delta
+	elif character.right_drift:
+		if character.input_right:
+			character.rotation.y -= drift_fine_yaw_rate * delta
+		elif character.input_left:
+			character.rotation.y = min(
+				character.rotation.y,
+				character.base_jump_yaw - MAX_FINE_BANK
+			)
+			character.rotation.y += drift_fine_yaw_rate * delta
